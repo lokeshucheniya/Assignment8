8763d11e35692c306abc148403d9c5fc3a9dba44
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 18:25:53 +0530

    Update TestPerson.java

--- TestPerson.java
+++ TestPerson.java
@@ -2,10 +2,11 @@
 public class TestPerson {
 
 	public static void main(String[] args) {
-		Person ram = new Person("Sample1",23,7,2010);
-		Person shyam = new Person("Sample2",12,2,2018);
-		ram.display();
-		shyam.display();
+		Person ram = new Person("Sample1",23,7,2010);		//Passing inputs to class Person
+		Person shyam = new Person("Sample2",12,2,2018);		//Passing inputs to class Person
+		//Calling method display
+		ram.display();						
+		shyam.display();	
 		shyam.olderOne(ram);
 	}
 
